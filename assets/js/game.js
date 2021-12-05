9202fe9b086daef5ab4f2adcbd6821a112086d4d
dmevola <dmevola@gmail.com>
2021-12-05 18:33:27 -0500

    fighting all robots once

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,12 +5,14 @@ var playerMoney = 10;
 
 // you can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
+     //fight function statements
+
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 // you want to fight or run?
@@ -66,4 +68,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 };
 
 // run fight function to start game
-fight()
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
